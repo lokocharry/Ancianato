b4263003418914ee5b510716de3bf2d3522b3f0c
Jorge Alberto Lozano Charry <jolberto_63@hotmail.com>
2015-11-29 09:51:43 -0500

    Eliminado Redactor

--- contenido/models.py
+++ contenido/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from redactor.fields import RedactorField
 from django.core.urlresolvers import reverse_lazy
 from froala_editor.fields import FroalaField
 from ckeditor_uploader.fields import RichTextUploadingField
